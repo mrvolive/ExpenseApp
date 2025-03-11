f6591f62e65a91e6ce67eee174622a389023ffd7
mrvoli <olivier.maraval@edu.univ-fcomte.fr>
2025-03-11 15:52:49 +0100

    navigation

--- ExpenseApp/ContentView.swift
+++ ExpenseApp/ContentView.swift
@@ -8,14 +8,50 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var selectedIndex: Int = 0
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        TabView(selection: $selectedIndex) {
+            NavigationStack() {
+                VStack {
+                    HStack{
+                        Button{
+                            
+                        } label: {
+                            Text("Edit")
+                        }
+                        Spacer()
+                        Button{
+                            
+                        } label: {
+                            Text("Add")
+                        }
+                    }
+                    .padding(EdgeInsets(top: 10, leading:20, bottom: 0, trailing:20))
+                    Spacer()
+                }
+                .navigationTitle("Expenses")
+            }
+            .tabItem {
+                Text("Expenses")
+                Image(systemName: "dollarsign.square")
+                    .renderingMode(.template)
+            }
+            .tag(0)
+            
+            NavigationStack() {
+                Text("Overview")
+                    .navigationTitle("Overview")
+                
+            }
+            .tabItem {
+                Text("Overview")
+                Image(systemName: "chart.bar")
+                
+            }
+            .badge("12")
+            .tag(1)
         }
-        .padding()
     }
 }
 
