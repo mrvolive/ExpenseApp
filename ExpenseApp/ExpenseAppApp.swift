2bc73cb4159873175ea7a1147d83493faf5e2ba6
mrvoli <olivier.maraval@edu.univ-fcomte.fr>
2025-03-11 15:21:39 +0100

    Initial Commit

---
+++ ExpenseApp/ExpenseAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseAppApp.swift
+//  ExpenseApp
+//
+//  Created by maraval olivier on 11/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
